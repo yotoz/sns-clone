72c6002edf524eef42bdfeb4e1228f0cbcc68a32
yotoz <thdrudwls7@gmail.com>
2020-08-24 02:18:05 +0900

    commit

--- src/components/SnsMain/index.js
+++ src/components/SnsMain/index.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+//components
+import SnsHeader from '../SnsHeader';
+
+//styles
 import './styles.scss';
 
+const Animation_Status = {
+  CLOSE: 0,
+  OPENNING: 1,
+  OPEN: 2,
+  CLOSING: 3,
+};
+
 const SnsMain = () => {
+  const [todoListData, setTodoListData] = useState([
+    {
+      isDone: false,
+      title: '',
+      details: '',
+      imgUri: '',
+    },
+  ]);
+
+  const [isExtends, setIsExtends] = useState(
+    Animation_Status.CLOSE,
+  );
+
+  const _addTodoListData = (title, details, imgUri) => {
+    setTodoListData(
+      todoListData.concat({
+        isDone: false,
+        title,
+        details,
+        imgUri,
+      }),
+    );
+  };
+
+  const changeExtendsState = (state) => {
+    setIsExtends(state);
+  };
+
   return (
     <div className="container">
-      <div className="sns-main"></div>
+      <SnsHeader
+        className="header-container"
+        onAddTodoListData={_addTodoListData}
+        isExtends={isExtends}
+        changeExtendsState={changeExtendsState}
+        Animation_Status={Animation_Status}
+      ></SnsHeader>
+      <div
+        className="body-container"
+        onClick={() => {
+          if (
+            !(
+              isExtends === Animation_Status.CLOSE ||
+              isExtends === Animation_Status.CLOSING
+            )
+          ) {
+            changeExtendsState(Animation_Status.CLOSING);
+          }
+        }}
+      >
+        <div className="sns-main">테스트 입니다</div>
+      </div>
     </div>
   );
 };
