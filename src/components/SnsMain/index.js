72b15c3dfc9ead2cea1d43f3828b38a8d4ca3ee7
yotoz <thdrudwls7@gmail.com>
2020-08-23 22:34:29 +0900

    Remove Strict Mode, Add Main Container

--- src/components/SnsMain/index.js
+++ src/components/SnsMain/index.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './styles.scss';
 
 const SnsMain = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="sns-main"></div>
+    </div>
+  );
 };
 
 export default SnsMain;
